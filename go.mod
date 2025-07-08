61fabc399000e88d74c0e07c28945a781fae3b4f
LaraBerenguer <laraberenguer@outlook.com>
2025-07-08 13:55:54 +0200

    Initi my first Go project with .gitignore and main package

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/LaraBerenguer/golang-project
+
+go 1.24.4
---
+++ index.go
@@ -0,0 +1,9 @@
+package main //main go package
+
+import (
+	"fmt" //different modules that affect our script. fmt is used as an exit function
+)
+
+func main() {
+	fmt.Println("Hello World!")
+}
