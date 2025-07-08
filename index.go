cf3e102f09ca371c9724d9d6c1a36229572eaae9
LaraBerenguer <laraberenguer@outlook.com>
2025-07-08 15:07:36 +0200

    Ex 5

--- index.go
+++ index.go
@@ -3,14 +3,43 @@ package main
 import "fmt"
 
 func main() {
-	number := 22
-	if number >= 0 && number <= 9 {
-		fmt.Println("The number has 1 digit")
-	} else if number >= 10 && number <= 99 {
-		fmt.Println("The number has 2 digits")
-	} else if number >= 100 && number <= 999 {
-		fmt.Println("The number has 3 digits")
-	} else {
-		fmt.Println("Number must be between 0 and 999")
+	switch "Summer" {
+	case "January", "March", "May", "July", "August", "October", "December":
+		fmt.Println("This month has 31 days")
+	case "April", "June", "September", "November":
+		fmt.Println("This month has 30 days")
+	case "February":
+		fmt.Println("This month has 28 days")
+	default:
+		fmt.Println("Please, enter a valid month")
+	}
+
+	switch 2 {
+	case 1:
+		fmt.Println("This month has 31 days")
+	case 2:
+		fmt.Println("This month has 28 days")
+	case 3:
+		fmt.Println("This month has 31 days")
+	case 4:
+		fmt.Println("This month has 30 days")
+	case 5:
+		fmt.Println("This month has 31 days")
+	case 6:
+		fmt.Println("This month has 30 days")
+	case 7:
+		fmt.Println("This month has 31 days")
+	case 8:
+		fmt.Println("This month has 31 days")
+	case 9:
+		fmt.Println("This month has 30 days")
+	case 10:
+		fmt.Println("This month has 31 days")
+	case 11:
+		fmt.Println("This month has 30 days")
+	case 12:
+		fmt.Println("This month has 31 days")
+	default:
+		fmt.Println("Please, enter a valid month")
 	}
 }
