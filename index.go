607750b9d26ba4de026e12a9adebbaffe8842e35
LaraBerenguer <laraberenguer@outlook.com>
2025-07-08 14:55:15 +0200

    Ex 4

--- index.go
+++ index.go
@@ -2,9 +2,15 @@ package main
 
 import "fmt"
 
-const ACADEMY = "Cibernarium"
-
 func main() {
-	text := "I'm studying at" + ACADEMY
-	fmt.Println(text)
+	number := 22
+	if number >= 0 && number <= 9 {
+		fmt.Println("The number has 1 digit")
+	} else if number >= 10 && number <= 99 {
+		fmt.Println("The number has 2 digits")
+	} else if number >= 100 && number <= 999 {
+		fmt.Println("The number has 3 digits")
+	} else {
+		fmt.Println("Number must be between 0 and 999")
+	}
 }
